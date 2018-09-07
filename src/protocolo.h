901e4e8a9f9129b06e8abff94ead836b9aac78ec
Emiliano Botti <emilianobottisuarez@gmail.com>
2018-09-07 14:51:21 -0300

    Se crea el protocolo como un .h aparte, para independizar del programa

---
+++ src/protocolo.h
@@ -0,0 +1,13 @@
+#if !defined(PROTOCOL_CONSTANTS_H)
+#define PROTOCOL_CONSTANTS_H 1
+
+// Bindings
+static const int PORT = 8888;
+
+// Definidos como ASCII
+static const char* INIT = "init";
+static const char* PLAY = "play";
+static const char* PAUSE = "pause";
+static const char* STOP = "stop";
+
+#endif
